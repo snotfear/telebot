4520f9c31ce601cf0b75811f932748268b940db0
snotfear <snotfear@yandex.ru>
2022-01-29 19:49:32 +0300

    ничего не менял

--- web_hook.py
+++ web_hook.py
@@ -3,4 +3,4 @@ import telebot
 bot = telebot.TeleBot("5069703088:AAEA-gfnn-tn9yy6i616eNtiV4sEUJa3x4E")
 
 bot.remove_webhook()
-bot.set_webhook("d5dh3ckod1smgtc6ovue.apigw.yandexcloud.net")
+# bot.set_webhook("d5dh3ckod1smgtc6ovue.apigw.yandexcloud.net")
